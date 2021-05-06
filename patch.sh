1d21b4cc938a173a292881e019cb0eb9d775e24c
Gui Andrade <gh@archshift.com>
2021-05-06 15:13:42 -0700

    Add patches for linux v5.10.19, with PCIE disabled

--- patch.sh
+++ patch.sh
@@ -3,26 +3,26 @@
 set -eou pipefail
 
 SCRIPT_DIR="$(dirname "$0")"
-B=icicle-kit-es
 
 PATCHES="\
-    $B/0001-PFSoC-Icicle-kit-Adding-DTS-makefile.patch \
-    $B/0001-Microchip-Polarfire-SoC-Clock-Driver.patch \
-    $B/0002-PFSoC-MAC-Interface-auto-negotiation.patch \
-    0001-V2-GPIO-Driver-updates.patch \
-    0003-Microchip-Adding-I2C-Support-for-the-Polarfire-SoC.patch \
-    0008-pac139x.patch \
-    v11-0004-PCI-microchip-Add-host-driver-for-Microchip-PCIe.patch \
-    v1-0002-Add-definition-for-Microchip-PolarFire-SoC.patch \
-    v1-0001-dt-bindings-CLK-microchip-Add-Microchip-PolarFire.patch \
-    0001-V5-Adding-Microchip-MUSB-Driver.patch \
-    0002-uio-can-Microchip-PolarFire-add-CAN-support-via-uio.patch \
-    0001-Microsemi-UIO-Fabric-DMA-support.patch \
-    $B/0003-mbox-add-polarfire-soc-system-controller-mailbox.patch \
-    $B/0002-atish-soc-support.patch \
-    $B/0001-rng-add-support-for-rng-on-the-polarfire-soc.patch \
-    $B/0005-soc-add-polarfire-soc-system-controller.patch \
-    $B/0007-sys-serv-fpga-digest-and-serial-num.patch \
+0001-PFSoC-Icicle-kit-Adding-DTS-makefile.patch
+0002-clk-microchip-Add-driver-for-Microchip-PolarFire-SoC.patch
+0003-PFSoC-MAC-Interface-auto-negotiation.patch
+0004-V2-GPIO-Driver-updates-Enabled-MSS-Clock-for-GPIO.patch
+0005-Microchip-Adding-I2C-Support-for-the-Polarfire-SoC.patch
+0006-iio-adc-pac193x-add-driver-support-for-PAC193x.patch
+0007-PCI-microchip-Add-host-driver-for-Microchip-PCIe-con.patch
+0008-Add-definition-for-Microchip-PolarFire-SoC.patch
+0009-dt-bindings-clk-microchip-Add-Microchip-PolarFire-ho.patch
+0010-V5-Adding-Microchip-MUSB-Driver.patch
+0011-uio-can-Microchip-PolarFire-add-CAN-support-via-uio.patch
+0012-Microsemi-UIO-Fabric-DMA-support.patch
+0013-mbox-add-polarfire-soc-system-controller-mailbox.patch
+0014-atish_roundup.patch
+0015-rng-add-support-for-rng-on-the-polarfire-soc.patch
+0016-soc-add-polarfire-soc-system-controller.patch
+0017-WIP-system-services-fpga-digest-and-serial-num.patch
+0018-Add-Keystone-DMA-memory-initialization.patch
 "
 
 for PATCH in $PATCHES; do
@@ -30,5 +30,5 @@ for PATCH in $PATCHES; do
     git am "$SCRIPT_DIR/patches/$PATCH"
 done
 
-cp "$SCRIPT_DIR/patches/$B/icicle-kit-es-a000-microchip.dts" arch/riscv/boot/dts/microchip/
-cp "$SCRIPT_DIR/patches/$B/defconfig" .config
+cp "$SCRIPT_DIR/patches/icicle-kit-es-a000-microchip.dts" arch/riscv/boot/dts/microchip/
+cp "$SCRIPT_DIR/patches/defconfig" .config
